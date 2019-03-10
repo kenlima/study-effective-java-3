73e4d9da8d0469a9d62ec33845f401df25b5c6bc
jwlee <kenlima2k@gmail.com>
2019-03-10 09:13:08 +0900

    first commit

---
+++ src/main/java/com/example/studyeffectivejava/StudyEffectiveJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyeffectivejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEffectiveJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyEffectiveJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyeffectivejava/BuilderPatternTest.java
@@ -0,0 +1,17 @@
+package com.example.studyeffectivejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuilderPatternTest {
+
+    @Test
+    public void builder_test() {
+        NutritionFacts cocaCola = new NutritionFacts.Builder(240, 8)
+                .calories(100).sodium(35).carbohydrate(27).build();
+    }
+}
